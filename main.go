ac9111e8c6a93d57e3bb8851d64e072b2e661bec
JT Olio <hello@jtolio.com>
2020-09-11 15:44:14 -0600

    drag this project out of the past

--- main.go
+++ main.go
@@ -4,20 +4,19 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"runtime"
+	"time"
+
+	"golang.org/x/crypto/acme/autocert"
 )
 
 var (
-	// staging server: https://acme-staging.api.letsencrypt.org/directory
-	// production server: https://acme-v01.api.letsencrypt.org/directory
-	apiServer = flag.String("letsencrypt.api_server",
-		"https://acme-v01.api.letsencrypt.org/directory",
-		"letsencrypt api server")
 	reachableHost = flag.String("host", "",
 		"the hostname to get a certificate for. something like "+
 			"'yourservice.yourdomain.tld'")
@@ -26,15 +25,30 @@ var (
 	targetAddr = flag.String("target", "",
 		"the address to forward unencrypted connections to. "+
 			"probably something like 'localhost:8080'")
-	statePath = flag.String("path",
-		os.ExpandEnv("$HOME/.lecat"),
-		"the path to a folder to keep state. If you already have a "+
-			"letsencrypt account key, please name it 'account.key' in this folder.")
+	statePath = flag.String("path", os.ExpandEnv("$HOME/.lecat"),
+		"the path to a folder to keep state.")
 	redirectAddr = flag.String("redirect-addr", "",
 		"if set, will listen on this address and redirect unencrypted http "+
-			"requests to it to https AND set HSTS for a year. if you want this at "+
+			"requests to it to https AND set HSTS. if you want this at "+
 			"all, you almost certainly want ':80'")
+	hstsDuration = flag.Duration("hsts-duration", 24*time.Hour,
+		"if redirect-addr is set, length of time to trigger hsts")
+	acceptTOS    = flag.Bool("accept-tos", false, "if true, accept lets encrypt TOS")
+	supportHTTP2 = flag.Bool("support-http2", false,
+		"if true, indicate http2 support in the ssl handshake")
+	email = flag.String("email", "", "the email address to use with letsencrypt")
 )
+
+func AcceptTOS(url string) bool {
+	if *acceptTOS {
+		return true
+	}
+	fmt.Println("Do you accept the Let's Encrypt terms of service?")
+	fmt.Println(url)
+	fmt.Println("Press enter to accept, control-c to reject")
+	_, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	return err == nil
+}
 
 func main() {
 	flag.Parse()
@@ -48,22 +62,19 @@ func main() {
 		log.Fatal("--target argument required")
 	}
 
+	manager := &autocert.Manager{
+		Prompt:     AcceptTOS,
+		HostPolicy: autocert.HostWhitelist(*reachableHost),
+		Email:      *email,
+	}
+
 	log.Println("loading configuration")
 
 	err := os.MkdirAll(*statePath, 0700)
 	if err != nil {
 		log.Fatal("failed to make state path:", err)
 	}
-
-	key, err := loadKey("server")
-	if err != nil {
-		log.Fatal("failed to load server key:", err)
-	}
-
-	cert, err := loadServerCert(key)
-	if err != nil {
-		log.Fatal("failed to load or create server cert:", err)
-	}
+	manager.Cache = autocert.DirCache(*statePath)
 
 	if *redirectAddr != "" {
 		go func() {
@@ -72,7 +83,7 @@ func main() {
 				func(w http.ResponseWriter, req *http.Request) {
 					log.Printf("redirecting request from %s and setting HSTS",
 						req.RemoteAddr)
-					w.Header().Set("Strict-Transport-Security", "max-age=31536000")
+					w.Header().Set("Strict-Transport-Security", fmt.Sprintf("max-age=%d", int64(hstsDuration.Seconds())))
 					http.Redirect(w, req,
 						fmt.Sprintf("https://%s%s", *reachableHost, req.RequestURI),
 						http.StatusMovedPermanently)
@@ -80,5 +91,5 @@ func main() {
 		}()
 	}
 
-	panic(serve(key, cert))
+	panic(serve(manager))
 }
